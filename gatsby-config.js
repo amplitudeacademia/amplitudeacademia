69d1bbd2d2d3c086aac19bbc67dc63a876649aa7
amplitudeacademia <48505170+amplitudeacademia@users.noreply.github.com>
2020-07-25 02:39:35 -0300

    Tinacms

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
             resolve: 'gatsby-plugin-tinacms',
             options: {
               // The CMS will be disabled on your production site
-              enabled: process.env.NODE_ENV !== 'production',
+              //enabled: process.env.NODE_ENV !== 'production',
               sidebar: true,
               plugins: [
                 // We'll add some gatsby-tinacms plugins later
